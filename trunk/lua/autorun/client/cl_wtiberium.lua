013fc5f22434731061dcb421913c586476dd6ca8
kevkevm <kevkevm@04c08556-1d1c-11de-b69a-45dc1c5e4070>
2009-04-12 20:24:12 +0000

    Moved all the commands to ClientConvars, Added WTiberium_UseTooltips to turn off the tooltips,

--- trunk/lua/autorun/client/cl_wtiberium.lua
+++ trunk/lua/autorun/client/cl_wtiberium.lua
@@ -1,19 +1,11 @@
 killicon.Add("wtib_missile","killicons/wtib_missile_killicon",Color(255,80,0,255))
 
 WTib_DynamicLight = CreateClientConVar("WTiberium_DynamicLights",1,true,true)
-WTib_DynamicLightSize = 1
-
-function WTib_DynamicLightsSizeConsole(ply,com,args)
-	if !args[1] then
-		print("Please specify the size!")
-		return
-	end
-	WTib_DynamicLightSize = tonumber(args[1])
-	LocalPlayer():ChatPrint("Dynamic lights size changed to "..tostring(WTib_DynamicLightSize))
-end
-concommand.Add("WTiberium_DynamicLightsSize",WTib_DynamicLightsSizeConsole)
+WTib_DynamicLightSize = CreateClientConVar("WTiberium_DynamicLightsSize",1,true,true)
+WTib_UseToolTips = CreateClientConVar("WTiberium_UseTooltips",1,true,true)
 
 function WTib_ToolTipDraw()
+	if !WTib_UseToolTips:GetBool() then return end
 	local tr = LocalPlayer():GetEyeTrace()
 	if tr.Hit and tr.Entity and tr.Entity.WTib_GetTooltip then
 		AddWorldTip(tr.Entity:EntIndex(),tr.Entity:WTib_GetTooltip(),0.5,tr.HitPos,self)
