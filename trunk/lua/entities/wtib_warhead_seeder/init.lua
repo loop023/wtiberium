74d8da485ea1c322fd4449845adae60f409f9a32
kevkevm <kevkevm@04c08556-1d1c-11de-b69a-45dc1c5e4070>
2009-04-16 18:39:09 +0000

    Beta effect,

--- trunk/lua/entities/wtib_warhead_seeder/init.lua
+++ trunk/lua/entities/wtib_warhead_seeder/init.lua
@@ -25,59 +25,45 @@ function ENT:SpawnFunction(p,t)
 end
 
 function ENT:Explode(missile,data)
-	print("Exploding")
 	data = data or {}
-	print("Printing data :")
-	PrintTable(data)
-	print("Data printed, effect starting")
 	local ed = EffectData()
 	ed:SetOrigin(data.HitPos or missile:GetPos())
 	ed:SetStart(data.HitPos or missile:GetPos())
-	util.Effect("SeederExplosion",ed)
-	print("Effect Created")
+	ed:SetScale(1)
+	util.Effect("WTib_SeederExplosion",ed)
 	local t = util.QuickTrace(missile:GetPos(),data.HitPos or missile:GetForward()*80,{missile,missile.FTrail})
-	print("Printing trace results")
-	PrintTable(t)
 	if (t.Entity and (t.Entity:IsPlayer() or t.Entity:IsNPC() or t.Entity.IsTiberium)) or t.HitSky then missile:Remove() return end
-	print("Check passed")
-	print("Creating ent")
 	local e = ents.Create("wtib_greentiberium")
-	print("Ent created")
 	local ang = t.HitNormal:Angle()+Angle(90,0,0)
-	print("Angle : "..tostring(ang))
 	ang:RotateAroundAxis(ang:Up(),math.random(0,360))
-	print("New angle : "..tostring(ang))
 	e:SetAngles(ang)
-	print("Angles set")
 	e:SetPos(t.HitPos)
-	print("Pos set")
 	e.WDSO = missile.WDSO
-	print("WDSO set")
 	e:Spawn()
-	print("Spawned")
 	e:Activate()
-	print("Activated")
 	if t.Entity and !t.Entity:IsWorld() then
-		print("Hit entity, setting movetype")
 		e:SetMoveType(MOVETYPE_VPHYSICS)
-		print("Movetype set, setting parent")
 		e:SetParent(t.Entity)
-		print("Parent sec")
 	end
-	print("Emitting gas")
 	for i=1,3 do
 		print(i)
 		e:EmitGas()
 	end
-	print("Producing")
 	for i=1,6 do
 		print(i)
 		e:SetTiberiumAmount(3000)
 		e:Reproduce()
 	end
-	print("Setting tiberium amount")
+	for _,v in pairs(ents.FindInSphere(self:GetPos(),600)) do
+		if v.IsTiberium then
+			local ed = EffectData()
+			ed:SetOrigin(v:GetPos())
+			ed:SetStart(v:GetPos())
+			ed:SetScale(1)
+			util.Effect("WTib_SeederExplosion",ed)
+		end
+	end
 	e:SetTiberiumAmount(math.Rand(200,500))
-	print("Removing missile")
 	missile:Remove()
 end
 
