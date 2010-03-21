4536d31ebd53d56e56d8a5b81ae11189036a4274
kevkevm <kevkevm@04c08556-1d1c-11de-b69a-45dc1c5e4070>
2010-03-21 10:19:08 +0000

    Added a medium TRIP module,

---
+++ lua/entities/wtib_tripmodule_medium/cl_init.lua
@@ -0,0 +1,19 @@
+include('shared.lua')
+
+function ENT:Draw()
+	self:DrawModel()
+end
+
+function ENT:WTib_GetTooltip()
+	local on = "Off"
+	if self.dt.Online then
+		on = "On"
+	end
+	return self.PrintName.." ("..on..")\nEnergy : "..math.Round(tostring(self.dt.Energy))
+end
+
+function ENT:Think()
+	self:NextThink(CurTime()+1)
+	return true
+end
+language.Add(string.Replace(ENT.Folder,"entities/",""),ENT.PrintName)
