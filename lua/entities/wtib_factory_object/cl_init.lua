f1e1e095b3d7036344ecea80c45f6c5e3d331d3a
kevkevm <kevkevm@04c08556-1d1c-11de-b69a-45dc1c5e4070>
2010-05-14 21:11:44 +0000

    Better model cliping,

--- lua/entities/wtib_factory_object/cl_init.lua
+++ lua/entities/wtib_factory_object/cl_init.lua
@@ -6,9 +6,13 @@ function ENT:Draw()
 	local Maxs = self:OBBMaxs().z
 	local distance = normal:Dot(self:LocalToWorld(Vector(0,0,Mins+(((Maxs-Mins)/100)*self.dt.Factory.dt.PercentageComplete))))
 	render.EnableClipping(true)
-	render.PushCustomClipPlane(normal,distance)
-	self:DrawModel()
-	render.PopCustomClipPlane()
+		render.PushCustomClipPlane(normal,distance)
+			render.CullMode(MATERIAL_CULLMODE_CW) 
+			self:DrawModel()
+			render.CullMode(MATERIAL_CULLMODE_CCW)
+			self:DrawModel()
+		render.PopCustomClipPlane()
+	render.EnableClipping(false)
 end
 
 function ENT:Think()
