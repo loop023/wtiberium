7c35a65bd15d9ccdf4d68d97c24266f8a0fe92fe
kevkevm <kevkevm@gmail.com>
2011-10-16 18:09:33 +0000

    Added the infection cure to the dispenser,

--- lua/entities/wtib_infectioncure/shared.lua
+++ lua/entities/wtib_infectioncure/shared.lua
@@ -7,3 +7,23 @@ ENT.Instructions	= ""
 ENT.Spawnable		= true
 ENT.AdminSpawnable	= true
 ENT.Category		= "Tiberium"
+
+WTib.Dispenser.AddObject({
+	Name = ENT.PrintName,
+	Class = WTib.GetClass(ENT),
+	Model = "models/Tiberium/tib_antidote.mdl",
+	PercentDelay = 0.02,
+	Information =	{
+						SWEP.PrintName,
+						"\nA vial that contains the antidote for tiberium infection."
+					},
+	CreateEnt = function(dispenser,angles,pos,id)
+		local ent = ents.Create(WTib.Dispenser.GetObjectByID(id).Class)
+		ent:SetPos(pos)
+		ent:SetAngles(angles)
+		ent:Spawn()
+		ent:Activate()
+		ent:SetModel(WTib.Dispenser.GetObjectByID(id).Model)
+		return ent
+	end
+})
