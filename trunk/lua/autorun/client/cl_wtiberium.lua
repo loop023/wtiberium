0a0d51f093c2cee9687e86d07b7ebac080b332d9
kevkevm@gmail.com <kevkevm@gmail.com@04c08556-1d1c-11de-b69a-45dc1c5e4070>
2012-04-11 21:12:00 +0000

    Changed all clientside convars to lowercase letters, Made the tooltip bubble system more error proof, Added the dispenser entity to the factory, Fixed the radar not draining energy,

--- trunk/lua/autorun/client/cl_wtiberium.lua
+++ trunk/lua/autorun/client/cl_wtiberium.lua
@@ -1,25 +1,28 @@
 
 WTib = WTib or {}
 
-WTib.DynamicLight = CreateClientConVar("WTib_DynamicLight",1,true,false)
-WTib.DynamicLightSize = CreateClientConVar("WTib_DynamicLightSize",1,true,false)
+WTib.DynamicLight = CreateClientConVar("wtib_dynamiclight",1,true,false)
+WTib.DynamicLightSize = CreateClientConVar("wtib_dynamiclightsize",1,true,false)
 
-WTib.UseToolTips = CreateClientConVar("WTib_UseTooltips",1,true,false)
-WTib.ToolTipsRange = CreateClientConVar("WTib_ToolTipsRange",512,true,false)
-WTib.UseOldToolTips = CreateClientConVar("WTib_UseOldTooltips",1,true,false)
+WTib.UseToolTips = CreateClientConVar("wtib_usetooltips",1,true,false)
+WTib.ToolTipsRange = CreateClientConVar("wtib_tooltipsrange",512,true,false)
+WTib.UseOldToolTips = CreateClientConVar("wtib_useoldtooltips",1,true,false)
 
 function WTib.ToolTipDraw()
 	if !WTib.UseToolTips:GetBool() then return end
 	local tr = LocalPlayer():GetEyeTrace()
 	if tr.Hit and tr.Entity and tr.Entity.WTib_GetTooltip and EyePos():Distance(tr.Entity:GetPos()) < WTib.ToolTipsRange:GetInt() then
+		
 		local p = tr.HitPos
 		if WTib.UseOldToolTips:GetBool() then
 			p = tr.Entity:GetPos()
 		end
-		local tip = tr.Entity:WTib_GetTooltip()
-		if tip and tip != "" then
-			AddWorldTip(tr.Entity:EntIndex(),tip,0.5,p,self)
-		end
+		
+		local tip = ""
+		local status, err = pcall(function() tip = tr.Entity:WTib_GetTooltip() end)
+		if !status then tip = err end
+		
+		AddWorldTip(tr.Entity:EntIndex(),tip,0.5,p,self)
 	end
 end
 hook.Add("HUDPaint","WTib.ToolTipDraw",WTib.ToolTipDraw)
--- trunk/lua/entities/wtib_dispenser/shared.lua
+++ trunk/lua/entities/wtib_dispenser/shared.lua
@@ -14,3 +14,33 @@ function ENT:SetupDataTables()
 	self:DTVar("Bool",0,"IsBuilding")
 	self:DTVar("Entity",0,"CurObject")
 end
+
+WTib.Factory.AddObject({
+	Name = ENT.PrintName,
+	Class = WTib.GetClass(ENT),
+	Model = "models/Tiberium/dispenser.mdl",
+	PercentDelay = 0.08,
+	Information =	{
+						ENT.PrintName,
+						"\nDispenses infantry weaponary."
+					},
+	CreateEnt = function( factory, angles, pos, id, ply )
+		local Obj = WTib.Factory.GetObjectByID(id)
+		local ent = ents.Create(Obj.Class)
+		ent:SetPos(pos)
+		ent:SetAngles(angles)
+		ent:Spawn()
+		ent:Activate()
+		ent:SetModel(Obj.Model)
+		
+		if ply then
+			ent.WDSO = ply
+			undo.Create(Obj.Name)
+				undo.AddEntity(ent)
+				undo.SetPlayer(ply)
+			undo.Finish()
+		end
+		
+		return ent
+	end
+})
