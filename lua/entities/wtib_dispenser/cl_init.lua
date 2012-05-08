523f0804af20da387f626f1815f086a61db58d17
kevkevm <kevkevm@gmail.com>
2012-05-08 22:25:39 +0000

    Updated the code to be only compatible with the new GMod beta, Optimized the dispenser screen code, Optimized the factory screen code, Sorting by assembly time in the factory/dispenser menu now works properly, Fixed the factory sometimes not opening the GUI and being locked out for 30 seconds, Added an error and ok sound to the factory when attempting to build and object/open the GUI, Added a few effects to the factory (Smoke comes out of the vents and there are small sprites on the factory laser towers that indicate the progress of the current object), Added a few comments to the code, Made the red Tiberium a lot more volatile (One explosion could wipe out the entire field), Tiberium now damages suit power first instead of damaging health and suit power at the same time,

--- lua/entities/wtib_dispenser/cl_init.lua
+++ lua/entities/wtib_dispenser/cl_init.lua
@@ -1,8 +1,14 @@
 include('shared.lua')
 
+local Color_White = Color(255,255,255,255)
+local ScreenVect = Vector(9,-15,24)
+local ScreenAng = Angle(0,90,90)
+
+local MainBox
+
 function ENT:Draw()
 	self:DrawModel()
-	cam.Start3D2D(self:LocalToWorld(Vector(9,-15,24)),self:LocalToWorldAngles(Angle(0,90,90)),0.15)
+	cam.Start3D2D(self:LocalToWorld(ScreenVect),self:LocalToWorldAngles(ScreenAng),0.15)
 		self:Draw3D2D()
 	cam.End3D2D()
 	WTib.Render(self)
@@ -18,8 +24,8 @@ function ENT:Draw3D2D()
 	if self.dt.PercentageComplete then
 		Text = "Working..."
 	end
-	draw.DrawText(Text,"Trebuchet18",1,4,Color(255,255,255,255),ALIGN_LEFT)
-	draw.DrawText("Completed : "..self.dt.PercentageComplete.."%","Trebuchet18",1,20,Color(255,255,255,255),ALIGN_LEFT)
+	draw.DrawText(Text,"Trebuchet18",1,4,Color_White,ALIGN_LEFT)
+	draw.DrawText("Completed : "..self.dt.PercentageComplete.."%","Trebuchet18",1,20,Color_White,ALIGN_LEFT)
 end
 
 function ENT:WTib_GetTooltip()
@@ -37,62 +43,100 @@ end
 language.Add(WTib.GetClass(ENT),ENT.PrintName)
 
 usermessage.Hook("wtib_dispenser_openmenu",function(um)
-	local Dispenser = um:ReadEntity()
-	local Selected
-	local MainBox = vgui.Create("DFrame")
-	MainBox:SetSize(400,350)
-	MainBox:SetPos((ScrW()/2)-200,(ScrH()/2)-175)
-	MainBox:SetTitle("Dispenser Menu")
-	MainBox:SetVisible(true)
-	MainBox:SetDraggable(false)
-	MainBox:ShowCloseButton(false)
-	MainBox:MakePopup()
-	
-	local ExitButton = vgui.Create("DButton")
-	ExitButton:SetParent(MainBox)
-	ExitButton:SetPos(80,310)
-	ExitButton:SetSize(100,30)
-	ExitButton:SetText("Close")
-	ExitButton.DoClick = function(self)
-		RunConsoleCommand("wtib_factory_closemenu",Dispenser:EntIndex())
-		MainBox:Remove()
-	end
+	if !MainBox or MainBox == NULL then
+		local Dispenser = um:ReadEntity()
+		local Selected
+		MainBox = vgui.Create("DFrame")
+		MainBox:SetSize(400,350)
+		MainBox:SetPos((ScrW()/2)-200,(ScrH()/2)-175)
+		MainBox:SetTitle("Dispenser Menu")
+		MainBox:SetVisible(true)
+		MainBox:SetDraggable(false)
 
-	local BuildButton = vgui.Create("DButton")
-	BuildButton:SetParent(MainBox)
-	BuildButton:SetPos(220,310)
-	BuildButton:SetSize(100,30)
-	BuildButton:SetText("Build")
-	BuildButton:SetDisabled(true)
-	BuildButton.DoClick = function(self)
-		RunConsoleCommand("wtib_dispenser_buildobject",Dispenser:EntIndex(),Selected)
-		RunConsoleCommand("wtib_dispenser_closemenu",Dispenser:EntIndex())
-		MainBox:Remove()
+		WTib.RemovePanelControls(MainBox)
+		
+		MainBox:MakePopup()
+		
+		local ExitButton = vgui.Create("DButton")
+		ExitButton:SetParent(MainBox)
+		ExitButton:SetPos(80,310)
+		ExitButton:SetSize(100,30)
+		ExitButton:SetText("Close")
+		ExitButton.DoClick = function(self)
+			net.Start("wtib_dispenser_closemenu")
+				net.WriteLong(Dispenser:EntIndex())
+			net.SendToServer()
+			MainBox:SetVisible(false)
+		end
+
+		local BuildButton = vgui.Create("DButton")
+		BuildButton:SetParent(MainBox)
+		BuildButton:SetPos(220,310)
+		BuildButton:SetSize(100,30)
+		BuildButton:SetText("Build")
+		BuildButton:SetDisabled(true)
+		BuildButton.DoClick = function(self)
+			net.Start("wtib_dispenser_buildobject")
+				net.WriteLong(Dispenser:EntIndex())
+				net.WriteEntity(LocalPlayer())
+				net.WriteFloat(Selected)
+			net.SendToServer()
+			net.Start("wtib_dispenser_closemenu")
+				net.WriteLong(Dispenser:EntIndex())
+			net.SendToServer()
+			MainBox:SetVisible(false)
+		end
+		
+		local BuildList = vgui.Create("DListView")
+		BuildList:SetParent(MainBox)
+		BuildList:SetPos(25,40)
+		BuildList:SetSize(350,260)
+		BuildList:SetMultiSelect(false)
+		BuildList.WTib_SortingWay = false
+		MainBox.BuildList = BuildList
+		local IDColum = BuildList:AddColumn("ID")
+		IDColum:SetMinWidth(20)
+		IDColum:SetMaxWidth(20)
+		BuildList:AddColumn("Entity")
+		local BColum = BuildList:AddColumn("Build Time")
+		BColum:SetMinWidth(60)
+		BColum:SetMaxWidth(60)
+		BColum.DoClick = function(panel) // Hate the way this is done, but its the only way AFAIK
+		
+			local TempList = BuildList:GetLines()
+			table.sort(TempList, function(a,b)
+				if BuildList.WTib_SortingWay then
+					return tonumber(string.Replace(a:GetValue(3), " Sec.","")) < tonumber(string.Replace(b:GetValue(3), " Sec.",""))
+				else
+					return tonumber(string.Replace(a:GetValue(3), " Sec.","")) > tonumber(string.Replace(b:GetValue(3), " Sec.",""))
+				end
+			end)
+			BuildList.WTib_SortingWay = !BuildList.WTib_SortingWay
+
+			BuildList:Clear()
+			for _,v in pairs(TempList) do
+				BuildList:AddLine(v:GetValue(1), v:GetValue(2), v:GetValue(3))
+			end
+			
+		end
+		BuildList.OnRowSelected = function(panel,line)
+			Selected = BuildList:GetLine(line):GetValue(1)
+			BuildButton:SetDisabled(false)
+		end
+		BuildList.DoDoubleClick = function(panel,line,list)
+			Selected = BuildList:GetLine(line):GetValue(1)
+			BuildButton.DoClick(BuildButton)
+		end
+		
+		for k,v in pairs(WTib.Dispenser.GetObjects()) do
+			BuildList:AddLine(k,v.Name,tostring(math.ceil(100*v.PercentDelay)).." Sec.")
+		end
+	else
+		MainBox.BuildList:Clear()
+		for k,v in pairs(WTib.Dispenser.GetObjects()) do
+			MainBox.BuildList:AddLine(k,v.Name,tostring(math.ceil(100*v.PercentDelay)).." Sec.")
+		end
+		
+		MainBox:SetVisible(true)
 	end
-	
-	local BuildList = vgui.Create("DListView")
-	BuildList:SetParent(MainBox)
-	BuildList:SetPos(25,40)
-	BuildList:SetSize(350,260)
-	BuildList:SetMultiSelect(false)
-	local IDColum = BuildList:AddColumn("ID")
-	IDColum:SetMinWidth(20)
-	IDColum:SetMaxWidth(20)
-	BuildList:AddColumn("Entity")
-	local BColum = BuildList:AddColumn("Build Time")
-	BColum:SetMinWidth(60)
-	BColum:SetMaxWidth(60)
-	BuildList.OnRowSelected = function(panel,line)
-		Selected = BuildList:GetLine(line):GetValue(1)
-		BuildButton:SetDisabled(false)
-	end
-	BuildList.DoDoubleClick = function(panel,line,list)
-		Selected = BuildList:GetLine(line):GetValue(1)
-		BuildButton.DoClick(BuildButton)
-	end
-	
-	for k,v in pairs(WTib.Dispenser.GetObjects()) do
-		BuildList:AddLine(k,v.Name,tostring(math.ceil(100*v.PercentDelay)).." Sec.")
-	end
-	
 end)
--- lua/entities/wtib_factory/cl_init.lua
+++ lua/entities/wtib_factory/cl_init.lua
@@ -1,6 +1,8 @@
 include('shared.lua')
 
 local EffectDelay = 0.1
+local LastFactory
+local MainBox
 
 ENT.NextEffect = 0
 
@@ -40,100 +42,137 @@ language.Add(WTib.GetClass(ENT),ENT.PrintName)
 
 usermessage.Hook("wtib_factory_openmenu",function(um)
 	local Factory = um:ReadEntity()
-	local Selected
-	local MainBox = vgui.Create("DFrame")
-	MainBox:SetSize(400,350)
-	MainBox:SetPos((ScrW()/2)-200,(ScrH()/2)-175)
-	MainBox:SetTitle("Factory Menu")
-	MainBox:SetVisible(true)
-	MainBox:SetDraggable(false)
-	MainBox:ShowCloseButton(false)
-	MainBox:MakePopup()
+	if Factory != LastFactory and MainBox and MainBox != NULL then MainBox:Remove() end
+	LastFactory = Factory
 	
-	local ExitButton = vgui.Create("DButton")
-	ExitButton:SetParent(MainBox)
-	ExitButton:SetPos(40,310)
-	ExitButton:SetSize(100,30)
-	ExitButton:SetText("Close")
-	ExitButton.DoClick = function(self)
-		RunConsoleCommand("wtib_factory_closemenu",Factory:EntIndex())
-		MainBox:Remove()
+	if !MainBox or MainBox == NULL or !MainBox.BuildList or MainBox.BuildList == NULL then
+		MainBox = vgui.Create("DFrame")
+		MainBox:SetSize(400,350)
+		MainBox:SetPos((ScrW()/2)-200,(ScrH()/2)-175)
+		MainBox:SetTitle("Factory Menu")
+		MainBox:SetVisible(true)
+		MainBox:SetDraggable(false)
+		
+		WTib.RemovePanelControls(MainBox)
+		
+		MainBox:MakePopup()
+		
+		local ExitButton = vgui.Create("DButton")
+		ExitButton:SetParent(MainBox)
+		ExitButton:SetPos(40,310)
+		ExitButton:SetSize(100,30)
+		ExitButton:SetText("Close")
+		ExitButton.DoClick = function(self)
+			MainBox:SetVisible(false)
+		end
+		
+		local InfoButton = vgui.Create("DButton")
+		InfoButton:SetParent(MainBox)
+		InfoButton:SetPos(150,310)
+		InfoButton:SetSize(100,30)
+		InfoButton:SetText("Info")
+		InfoButton:SetDisabled(true)
+		InfoButton.DoClick = function(self)
+			WTib_Factory_InfoMenu(MainBox.BuildList:GetLine(MainBox.BuildList:GetSelectedLine()):GetValue(1))
+		end
+		
+		local BuildButton = vgui.Create("DButton")
+		BuildButton:SetParent(MainBox)
+		BuildButton:SetPos(260,310)
+		BuildButton:SetSize(100,30)
+		BuildButton:SetText("Build")
+		BuildButton:SetDisabled(true)
+		BuildButton.DoClick = function(self)
+			WTib_Factory_StartBuild(Factory, MainBox.BuildList:GetLine(MainBox.BuildList:GetSelectedLine()):GetValue(1))
+			MainBox:SetVisible(false)
+		end
+		
+		local BuildList = vgui.Create("DListView")
+		BuildList:SetParent(MainBox)
+		BuildList:SetPos(25,40)
+		BuildList:SetSize(350,260)
+		BuildList:SetMultiSelect(false)
+		BuildList.WTib_SortingWay = false
+		MainBox.BuildList = BuildList
+		local IDColum = BuildList:AddColumn("ID")
+		IDColum:SetMinWidth(20)
+		IDColum:SetMaxWidth(20)
+		BuildList:AddColumn("Entity")
+		local BColum = BuildList:AddColumn("Build Time")
+		BColum:SetMinWidth(60)
+		BColum:SetMaxWidth(60)
+		BColum.DoClick = function(panel) // Hate the way this is done, but its the only way AFAIK
+		
+			local TempList = BuildList:GetLines()
+			table.sort(TempList, function(a,b)
+				if BuildList.WTib_SortingWay then
+					return tonumber(string.Replace(a:GetValue(3), " Sec.","")) < tonumber(string.Replace(b:GetValue(3), " Sec.",""))
+				else
+					return tonumber(string.Replace(a:GetValue(3), " Sec.","")) > tonumber(string.Replace(b:GetValue(3), " Sec.",""))
+				end
+			end)
+			BuildList.WTib_SortingWay = !BuildList.WTib_SortingWay
+
+			BuildList:Clear()
+			for _,v in pairs(TempList) do
+				BuildList:AddLine(v:GetValue(1), v:GetValue(2), v:GetValue(3))
+			end
+			
+		end
+		BuildList.OnRowSelected = function(panel,line)
+			InfoButton:SetDisabled(false)
+			BuildButton:SetDisabled(false)
+		end
+		BuildList.DoDoubleClick = function(panel,line,list)
+			WTib_Factory_StartBuild(Factory, MainBox.BuildList:GetLine(MainBox.BuildList:GetSelectedLine()):GetValue(1))
+			MainBox:SetVisible(false)
+		end
+		
+		for k,v in pairs(WTib.Factory.GetObjects()) do
+			BuildList:AddLine(k,v.Name,tostring(math.ceil(1000*v.PercentDelay) / 10).." Sec.")
+		end
+	else // The menu still exists, refresh the items list and show it again
+		MainBox.BuildList:Clear()
+		for k,v in pairs(WTib.Factory.GetObjects()) do
+			MainBox.BuildList:AddLine(k,v.Name,tostring(math.ceil(1000*v.PercentDelay) / 10).." Sec.")
+		end
+		
+		MainBox:SetSize(400,350)
+		MainBox:SetPos((ScrW()/2)-200,(ScrH()/2)-175)
+		MainBox:SetVisible(true)
 	end
-	
-	local InfoButton = vgui.Create("DButton")
-	InfoButton:SetParent(MainBox)
-	InfoButton:SetPos(150,310)
-	InfoButton:SetSize(100,30)
-	InfoButton:SetText("Info")
-	InfoButton:SetDisabled(true)
-	InfoButton.DoClick = function(self)
-		WTib_Factory_InfoMenu(Selected)
-	end
-	
-	local BuildButton = vgui.Create("DButton")
-	BuildButton:SetParent(MainBox)
-	BuildButton:SetPos(260,310)
-	BuildButton:SetSize(100,30)
-	BuildButton:SetText("Build")
-	BuildButton:SetDisabled(true)
-	BuildButton.DoClick = function(self)
-		RunConsoleCommand("wtib_factory_buildobject",Factory:EntIndex(),Selected)
-		RunConsoleCommand("wtib_factory_closemenu",Factory:EntIndex())
-		MainBox:Remove()
-	end
-	
-	local BuildList = vgui.Create("DListView")
-	BuildList:SetParent(MainBox)
-	BuildList:SetPos(25,40)
-	BuildList:SetSize(350,260)
-	BuildList:SetMultiSelect(false)
-	local IDColum = BuildList:AddColumn("ID")
-	IDColum:SetMinWidth(20)
-	IDColum:SetMaxWidth(20)
-	BuildList:AddColumn("Entity")
-	local BColum = BuildList:AddColumn("Build Time")
-	BColum:SetMinWidth(60)
-	BColum:SetMaxWidth(60)
-	BuildList.OnRowSelected = function(panel,line)
-		Selected = BuildList:GetLine(line):GetValue(1)
-		InfoButton:SetDisabled(false)
-		BuildButton:SetDisabled(false)
-	end
-	BuildList.DoDoubleClick = function(panel,line,list)
-		Selected = BuildList:GetLine(line):GetValue(1)
-		BuildButton.DoClick(BuildButton)
-	end
-	
-	for k,v in pairs(WTib.Factory.GetObjects()) do
-		BuildList:AddLine(k,v.Name,tostring(math.ceil(100*v.PercentDelay)).." Sec.")
-	end
-	
 end)
 
-function WTib_Factory_InfoMenu(id)
-	local Object = WTib.Factory.GetObjectByID(id)
+function WTib_Factory_StartBuild(Factory, ProjectID)
+	net.Start("wtib_factory_buildobject")
+		net.WriteLong(LocalPlayer():EntIndex())
+		net.WriteEntity(Factory)
+		net.WriteFloat(ProjectID)
+	net.SendToServer()
+end
+
+function WTib_Factory_InfoMenu(ProjectID)
+	local Object = WTib.Factory.GetObjectByID(ProjectID)
 	local InfoBox = vgui.Create("DFrame")
 	InfoBox:SetSize(400,350)
 	InfoBox:SetPos((ScrW()/2)-200,(ScrH()/2)-175)
 	InfoBox:SetTitle("Info "..Object.Name)
 	InfoBox:SetVisible(true)
 	InfoBox:SetDraggable(false)
-	InfoBox:ShowCloseButton(false)
-	InfoBox:MakePopup()
 	
-	local InfoList = vgui.Create("DPanelList")
-	InfoList:SetParent(InfoBox)
-	InfoList:SetPos(25,40)
-	InfoList:SetSize(350,260)
-	InfoList:SetSpacing(5)
-	InfoList:SetPadding(5)
-	InfoList:EnableHorizontal(false)
-	InfoList:EnableVerticalScrollbar(true)
+	WTib.RemovePanelControls(InfoBox)
+	
+	InfoBox:MakePopup()
+
+	local InfoField = vgui.Create("DTextEntry")
+	InfoField:SetParent(InfoBox)
+	InfoField:SetMultiline(true)
+	InfoField:SetEditable(false)
+	InfoField:SetPos(25,40)
+	InfoField:SetSize(350,260)
+	
 	for _,v in pairs(Object.Information) do
-		local Lab = vgui.Create("DLabel")
-		Lab:SetText(v)
-		Lab:SizeToContents()
-		InfoList:AddItem(Lab)
+		InfoField:SetValue(InfoField:GetValue() .. "\n" .. v)
 	end
 	
 	local ExitButton = vgui.Create("DButton")
