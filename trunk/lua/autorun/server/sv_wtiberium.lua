c6e8013f2070ee40caeba11cf428dfaee778d3d4
kevkevm@gmail.com <kevkevm@gmail.com@04c08556-1d1c-11de-b69a-45dc1c5e4070>
2012-09-28 14:32:30 +0000

    The Tiberium infection now damages health only, suit armor no longer blocks it,

--- trunk/lua/autorun/server/sv_wtiberium.lua
+++ trunk/lua/autorun/server/sv_wtiberium.lua
@@ -241,7 +241,7 @@ function WTib.Disenfect(ent)
 end
 hook.Add("PlayerSpawn","WTib.Disenfect",WTib.Disenfect)
 
-timer.Create("WTib.InfectedTimer",1,0,function()
+timer.Create("WTib.InfectedTimer", 1, 0, function()
 
 	local Crystal = ents.FindByClass("wtib_tiberiuminfection")[1]
 	if !WTib.IsValid(Crystal) then
@@ -262,8 +262,14 @@ timer.Create("WTib.InfectedTimer",1,0,function()
 			if IsValid(v.Attacker) then dmginfo:SetAttacker(v.Attacker) end
 			if IsValid(v.Inflictor) then dmginfo:SetInflictor(v.Inflictor) end
 			
+			// The infection is already under the Armor, so only the health will be drained
+			local Armor = v.Ent:IsPlayer() and v.Ent:Armor() or 0
+			v.Ent:SetArmor(0)
+			
 			dmginfo:SetDamage(math.random(v.DamageMin or 1, v.DamageMax or 3))
 			v.Ent:TakeDamageInfo(dmginfo)
+			
+			if Armor > 0 then v.Ent:SetArmor(Armor) end
 			
 		end
 
