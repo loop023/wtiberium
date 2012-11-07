683c30b60bb209bc499cd689be024be6f0b7a386
kevkevm@gmail.com <kevkevm@gmail.com@04c08556-1d1c-11de-b69a-45dc1c5e4070>
2012-11-07 17:05:10 +0000

    Disabled the energy need of the sonic generator (For testing purposes), Added a simple sphere effect on the sonic generator when its online,

--- trunk/lua/entities/wtib_sonicgenerator/cl_init.lua
+++ trunk/lua/entities/wtib_sonicgenerator/cl_init.lua
@@ -1,8 +1,20 @@
 include('shared.lua')
 
+local EffectColor = Color(255, 255, 255, 255)
+local EffectMat = Material("models/effects/portalrift_sheet")
+
 function ENT:Draw()
+	
 	self:DrawModel()
 	WTib.Render(self)
+	
+	if self.dt.Online then
+	
+		local Normal = (LocalPlayer():GetPos()-self:GetPos()):GetNormalized()
+		render.SetMaterial(EffectMat)
+		render.DrawSphere( self:GetPos(), 35, 20, 20, EffectColor )
+	
+	end
 end
 
 function ENT:WTib_GetTooltip()
