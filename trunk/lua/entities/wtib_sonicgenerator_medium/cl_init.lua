2fbbaa174b454d03b3cc57cdc5997a6c05a74f5d
kevkevm@gmail.com <kevkevm@gmail.com@04c08556-1d1c-11de-b69a-45dc1c5e4070>
2012-11-08 16:30:36 +0000

    The sonic generator effect now pulsates, Removed some Tortoise SVN comments in the .vmt file,

--- trunk/lua/entities/wtib_sonicgenerator_medium/cl_init.lua
+++ trunk/lua/entities/wtib_sonicgenerator_medium/cl_init.lua
@@ -3,6 +3,13 @@ include('shared.lua')
 local EffectColor = Color(200, 200, 255, 255)
 local EffectMat = Material("wtiberium/effects/sonicgenerator")
 
+local EffectMaxSize = 33
+local EffectMinSize = 32
+
+ENT.EffectGrowing = true
+ENT.LastCurTime = 0
+ENT.EffectSize = 32
+
 function ENT:Draw()
 	
 	self:DrawModel()
@@ -12,7 +19,7 @@ function ENT:Draw()
 	
 		local Normal = (LocalPlayer():GetPos()-self:GetPos()):GetNormalized()
 		render.SetMaterial(EffectMat)
-		render.DrawSphere( self:GetPos(), 32, 35, 35, EffectColor )
+		render.DrawSphere( self:GetPos(), self.EffectSize, 35, 35, EffectColor )
 	
 	end
 	
@@ -27,7 +34,19 @@ function ENT:WTib_GetTooltip()
 end
 
 function ENT:Think()
-	self:NextThink(CurTime()+1)
+
+	if CurTime() != self.LastCurTime then // So that the effect stops when the game is pauzed
+	
+		local Target = self.EffectGrowing and EffectMaxSize or EffectMinSize
+		self.EffectSize = math.Approach( self.EffectSize, Target, 0.3)
+		if self.EffectSize == Target then self.EffectGrowing = !self.EffectGrowing end
+		
+	end
+	
+	self.LastCurTime = CurTime()
+	
+	self:NextThink(CurTime())
 	return true
+	
 end
 language.Add(WTib.GetClass(ENT),ENT.PrintName)
