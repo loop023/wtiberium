5fa1c3645cd81aa842cf2d7bb4fa521b46101088
kevkevm@gmail.com <kevkevm@gmail.com@04c08556-1d1c-11de-b69a-45dc1c5e4070>
2012-11-19 22:24:32 +0000

    Added anti aliasing to the screens of the dispenser and factory, Added an option to control the anti aliasing level in the menu,

--- trunk/lua/entities/wtib_factory_panel/cl_init.lua
+++ trunk/lua/entities/wtib_factory_panel/cl_init.lua
@@ -1,44 +1,51 @@
 include('shared.lua')
 
-local Color_White = Color(255,255,255,255)
-local ScreenVect = Vector(-8,-17,73.6)
-local ScreenAng = Angle(0,90,90)
+local Color_White = Color(255, 255, 255, 255)
+local ScreenVect = Vector(-8, -17, 73.6)
+local ScreenAng = Angle(0, 90, 90)
 
 function ENT:Draw()
 	self:DrawModel()
-	cam.Start3D2D(self:LocalToWorld(ScreenVect),self:LocalToWorldAngles(ScreenAng),0.25)
+	
+	cam.Start3D2D(self:LocalToWorld(ScreenVect), self:LocalToWorldAngles(ScreenAng), 0.25)
 		self:Draw3D2D()
 	cam.End3D2D()
+	
 end
 
 function ENT:Draw3D2D()
 
-	// Black background
-	surface.SetDrawColor(0,0,0,255)
-	surface.DrawRect(0,0,136,100)
+	render.PushFilterMin(WTib.AntiAliasingLevel:GetInt())
+    render.PushFilterMag(WTib.AntiAliasingLevel:GetInt())
 
-	// The percentage completed
-	local Text = "Idle"
-	local Percent = 0
-	local CurProj = "None"
-	
-	if WTib.IsValid(self.dt.Factory) then
-		Percent = self.dt.Factory.dt.PercentageComplete
-		if self.dt.Factory.dt.IsBuilding then
-			Text = "Working, "..Percent.."%"
+		// Black background
+		surface.SetDrawColor(0, 0, 0, 255)
+		surface.DrawRect(0, 0, 136, 100)
+
+		// The percentage completed
+		local Text = "Idle"
+		local CurProj = "None"
+		
+		if WTib.IsValid(self.dt.Factory) and self.dt.Factory.dt.IsBuilding then
+		
+			Text = "Working, " .. self.dt.Factory.dt.PercentageComplete .. "%"
 			
 			CurProj = ""
-			for k,v in pairs(string.Explode("",WTib.Factory.GetObjectByID(self.dt.Factory.dt.BuildingID).Name)) do
+			for k,v in pairs(string.Explode("", WTib.Factory.GetObjectByID(self.dt.Factory.dt.BuildingID).Name)) do
+			
 				CurProj = CurProj .. v
-				if (k % 22) == 0 then CurProj = CurProj .. "\n" end
+				if (k % 19) == 0 then CurProj = CurProj .. "\n" end
+				
 			end
 			
 		end
-	end
+		
+		draw.DrawText(Text, "Trebuchet18", 1, 4, Color_White,ALIGN_LEFT)
+		draw.DrawText("Current project :\n" .. CurProj,"Trebuchet18", 1, 36, Color_White,ALIGN_LEFT)
 	
-	draw.DrawText(Text,"Trebuchet18",1,4,Color_White,ALIGN_LEFT)
-	draw.DrawText("Current project :\n" .. CurProj,"Trebuchet18",1,36,Color_White,ALIGN_LEFT)
+	render.PopFilterMin()
+    render.PopFilterMag()
 	
 end
 
-language.Add(WTib.GetClass(ENT),ENT.PrintName)
+language.Add(WTib.GetClass(ENT), ENT.PrintName)
