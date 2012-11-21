35fb9bbc38ea62ddb1f45f11418d61483998ab93
kevkevm <kevkevm@gmail.com>
2012-11-21 22:47:55 +0000

    Added a check to make sure no one is spawning invalid entities through the stool,

--- lua/weapons/gmod_tool/stools/wtib_tool_storage.lua
+++ lua/weapons/gmod_tool/stools/wtib_tool_storage.lua
@@ -18,7 +18,23 @@ function TOOL:LeftClick(tr)
 	if !tr.Hit then return false end
 	
 	local Class = self:GetClientInfo( "type" )
-	if Class == "" then return false end
+	local ValidClass = false
+	
+	for k,v in pairs(list.Get("WTib_Tools_Storage")) do
+		
+		if v.wtib_tool_storage_type == Class then
+		
+			ValidClass = true
+			break
+		
+		end
+		
+	end
+	
+	if !ValidClass then
+		self:GetOwner():SendLua([[notification.AddLegacy( "Invalid storage unit, please select a storage unit from the menu", NOTIFY_ERROR, 5 ); surface.PlaySound( "buttons/button10.wav" )]])
+		return false
+	end
 
 	local ent = WTib.SpawnFunction( self:GetOwner(), tr, Class )
 	
